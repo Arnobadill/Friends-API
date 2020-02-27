4ca02b1daf2d28116d5fdec17380aa39ec45d508
Tcl_5 <Ahasan Adil Arnob>
2020-02-27 18:28:36 +0600

    first commit

---
+++ Data/FriendsDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Friends.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friends.Data
+{
+    public class FriendsDataContext : DbContext
+    {
+        public FriendsDataContext(DbContextOptions<FriendsDataContext> options)
+           : base(options)
+        {
+        }
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
